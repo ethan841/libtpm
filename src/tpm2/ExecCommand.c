6cdb4cd08c4b23d6c1941e20e6928c91bafee048
ethan841 <ethan841@gmail.com>
2022-05-02 15:33:58 +0900

    5/2 test

--- src/tpm2/ExecCommand.c
+++ src/tpm2/ExecCommand.c
@@ -211,7 +211,8 @@ ExecuteCommand(
     else
 #endif
     // TPM command.code logging
-    FILE *tpm_cmd = fopen("/home/mobileosdcap1/SGX/VM/tpm_cmdline.log", "w");
+
+    FILE *tpm_cmd = fopen("/home/mobileosdcap1/SGX/VM/tpm_cmdline.log", "a");
     
     fprintf(tpm_cmd, "%u\n", command.code); 
     
